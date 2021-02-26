fbfb2943ee2214f51a45257ea220e912722805b5
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-02-27 00:15:09 +0200

    Add sample of tickets shopping cart

---
+++ data/tickets.js
@@ -0,0 +1,18 @@
+const tickets = [
+    {
+        id: 1,
+        title: "Space Between Us",
+        cinema: "Cinema 1",
+        date: "2021-02-22",
+        time: "09:10",
+        amount: 2
+    },
+    {
+        id: 2,
+        title: "John Wick",
+        cinema: "Cinema 4",
+        date: "2021-02-22",
+        time: "11:30",
+        amount: 1,
+    },
+];
