2fadd2d03bac4da45f9b48d031776b81f1d7945c
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-03-08 00:40:03 +0200

    Add posibillity to save selected tickets to shopping cart

--- data/tickets.js
+++ data/tickets.js
@@ -5,7 +5,7 @@ const tickets = [
         cinema: "Cinema 1",
         date: "2021-02-22",
         time: "09:10",
-        amount: 2
+        amount: 2,
     },
     {
         id: 2,
@@ -16,3 +16,15 @@ const tickets = [
         amount: 1,
     },
 ];
+
+/**
+ * Naujas formatas
+const tickets = {
+    "1;Cinema 1;2021-02-22;09:10" : { 
+        quantity: 2,
+    },
+    "2;Cinema 4;2021-02-22;11:30" : {
+        quantity: 1,
+    },
+};
+ */
