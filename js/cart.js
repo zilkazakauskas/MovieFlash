39fcacdd5b4e596b77960728a65d09d96d701565
Rėdas Peškaitis <redas.peskaitis@gmail.com>
2021-03-09 01:48:12 +0200

    Check cart storage on page load

--- js/cart.js
+++ js/cart.js
@@ -1,10 +1,6 @@
 import movies, { index } from '../data/movies.js'
 import CartStorage from './CartStorage.js'
 
-const cartStorage = CartStorage.instance('tickets');
-const cartTickets = cartStorage.store;
-console.log(cartTickets);
-
 //bilietu listas
 const ul = document.getElementById("ticketsList");
 const emptyMsg = document.querySelector(".cartTickets");
@@ -14,16 +10,35 @@ const btnClearCart = document.getElementById("btnClearCart");
 
 // bilietu listo generavimas
 
-Object.entries(cartTickets).forEach(ticket => {
-    const [key, value] = ticket;
-    const [id, cinema, date, time] = key.split(';');
-    const { quantity } = value;
-    const idx = index[id];
-    const { title } = movies[idx];
-    const li = document.createElement("li");
-    li.setAttribute('className', 'tickets');
-    li.setAttribute('data-key', key);
-    li.innerHTML = `
+function storageIsEmpty() {
+    ul.innerHTML = '';
+    emptyMsg.innerHTML = "<h3 style='color: white; text-align: center'>Cart is empty!</h3>"
+    btnClearCart.setAttribute('class', 'btnHidden');
+    btnCheckout.setAttribute('class', 'btnHidden');
+}
+
+function createPage() {
+
+    const cartStorage = CartStorage.instance('tickets');
+    const cartTickets = cartStorage.store;
+    console.log(cartTickets);
+
+    if (Object.keys(cartTickets).length < 1) {
+        storageIsEmpty();
+        return;
+    }
+
+
+    Object.entries(cartTickets).forEach(ticket => {
+        const [key, value] = ticket;
+        const [id, cinema, date, time] = key.split(';');
+        const { quantity } = value;
+        const idx = index[id];
+        const { title } = movies[idx];
+        const li = document.createElement("li");
+        li.setAttribute('className', 'tickets');
+        li.setAttribute('data-key', key);
+        li.innerHTML = `
         Movie tittle: ${title}<br />
         Cinema: ${cinema}<br />
         Date: ${date}<br />
@@ -35,59 +50,57 @@ Object.entries(cartTickets).forEach(ticket => {
         <hr />
     `;
 
-    ul.appendChild(li);
-});
+        ul.appendChild(li);
+    });
 
-function handleBtnClick(event) {
-    const key = event.target.getAttribute('data-key');
-    const span = document.querySelector(`span[data-key="${key}"]`);
-    const action = event.target.getAttribute('data-action');
-    let quantity = parseInt(span.innerHTML);
+    function handleBtnClick(event) {
+        const key = event.target.getAttribute('data-key');
+        const span = document.querySelector(`span[data-key="${key}"]`);
+        const action = event.target.getAttribute('data-action');
+        let quantity = parseInt(span.innerHTML);
 
-    if (action == 'plus') {
-        quantity++;
-    } else if (action == 'minus') {
-        quantity--;
-    } else if (action == 'remove') {
-        quantity = 0;
+        if (action == 'plus') {
+            quantity++;
+        } else if (action == 'minus') {
+            quantity--;
+        } else if (action == 'remove') {
+            quantity = 0;
+        }
+
+        if (quantity < 1) {
+            const li = document.querySelector(`li[data-key="${key}"]`);
+            li.innerHTML = "";
+            cartStorage.removeItem(key);
+            return;
+        }
+        span.textContent = `${quantity}`;
+        cartStorage.setItem(key, { quantity });
     }
 
-    if (quantity < 1) {
-        const li = document.querySelector(`li[data-key="${key}"]`);
-        li.innerHTML = "";
-        cartStorage.removeItem(key);
-        return;
-    }
-    span.textContent = `${quantity}`;
-    cartStorage.setItem(key, { quantity });
+    ul.querySelectorAll('button').forEach(button => {
+        button.addEventListener("click", handleBtnClick)
+    });
+
+    btnClearCart.addEventListener("click", function () {
+        swal({
+            title: "Are you sure want to clear the cart?",
+            icon: "warning",
+            buttons: ["Cancel", "Yes"],
+            dangerMode: true,
+        }).then((willDelete) => {
+            if (willDelete) {
+                storageIsEmpty();
+                cartStorage.clear();
+            }
+        });
+    });
+
+    btnCheckout.addEventListener("click", function () {
+        swal("Thank you for purchase!");
+        storageIsEmpty();
+        cartStorage.clear();
+    });
+
 }
 
-ul.querySelectorAll('button').forEach(button => {
-    button.addEventListener("click", handleBtnClick)
-});
-
-btnClearCart.addEventListener("click", function () {
-    swal({
-        title: "Are you sure want to clear the cart?",
-        icon: "warning",
-        buttons: ["Cancel", "Yes"],
-        dangerMode: true,
-    }).then((willDelete) => {
-        if (willDelete) {
-            ul.innerHTML = '';
-            emptyMsg.innerHTML = "<h3 style='color: white; text-align: center'>Cart is empty!</h3>"
-            cartStorage.clear();
-            btnClearCart.setAttribute('class', 'btnHidden');
-            btnCheckout.setAttribute('class', 'btnHidden');
-        }
-    });
-});
-
-btnCheckout.addEventListener("click", function () {
-    swal("Thank you for purchase!");
-    ul.innerHTML = '';
-    emptyMsg.innerHTML = "<h3 style='color: white; text-align: center'>Cart is empty!</h3>"
-    cartStorage.clear();
-    btnClearCart.setAttribute('class', 'btnHidden');
-    btnCheckout.setAttribute('class', 'btnHidden');
-});
+createPage();
